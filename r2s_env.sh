26de366981879314a8377fb4ea2b9a45ba6bd5b0
Marcel Torne Villasevil <44652334+MarcelTorne@users.noreply.github.com>
2024-04-09 10:23:59 -0400

    Delete r2s_env.sh

--- r2s_env.sh
+++
@@ -1,9 +0,0 @@
-# set variables as you wish + source this from the root directory!
-
-# directory for saving wandb
-# export WANDB_DIR=/data/scratch-oc40/pulkitag/marcel/
-# export WANDB_DIR=/data/scratch-oc40/pulkitag/anthony/wandb
-export WANDB_DIR=/home/anthony/wandb
-
-# update python path (instead of `conda develop dependences`)
-export PYTHONPATH=$PYTHONPATH:$PWD/dependencies
